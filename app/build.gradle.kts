60bfcd90bbdad922a31acbb7ba87bbb68c2b4d26
Anton Chaiko <smart.just4fun@gmail.com>
2026-05-26 04:39:07 +0300

    AIAP-9: add koin implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,10 +23,16 @@ android {
     }
 }
 dependencies {
-    implementation(project(":core:design"))
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.koin.android)
+    implementation(libs.koin.compose)
+
+
+    implementation(project(":core:design"))
+    implementation(project(":core:di"))
 }
---
+++ core/di/src/main/java/com/example/di/KoinInitializer.kt
@@ -0,0 +1,17 @@
+package com.example.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
+
+fun initKoin(
+    app: Application,
+    appDeclaration: KoinAppDeclaration = {}
+) = startKoin {
+    androidContext(app)
+    appDeclaration()
+    modules(
+        appModule
+    )
+}
